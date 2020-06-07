52ef5cb977911688c9d5a5648b51b86405b74588
jc8080 <jc8080@rit.edu>
2020-06-07 15:45:31 -0400

    Added a h1 title as well as 3 buttons connecting to rock, paper, scissors. Made a basic layout for the positions of the buttons

--- js/script.js
+++ js/script.js
@@ -13,43 +13,43 @@ function generateComputer(){
 }
 
 //Checks player's choice and compares to computer
-function RPS(playerChoice, player){
+function RPS(player){
     let compChoice;
     const compNum = generateComputer();
     if (compNum == 1){
         compChoice = "rock"
         if(player.toLowerCase() == "rock"){
-            console.log("tie")
+            alert("tie")  
         }
         else if(player.toLowerCase() == "paper"){
-            console.log("Player Wins")
+            alert("Player Wins")
         }
         else{
-            console.log("Computer Wins")
+            alert("Computer Wins")
         }
     }
     else if(compNum == 2){
         compChoice = "paper"
         if(player.toLowerCase() == "rock"){
-            console.log("Computer Wins")
+            alert("Computer Wins")
         }
         else if(player.toLowerCase() == "paper"){
-            console.log("Tie")
+            alert("Tie")
         }
         else{
-            console.log("Player Wins")
+            alert("Player Wins")
         }
     }
     else{
         compChoice = "scissors"
         if(player.toLowerCase() == "rock"){
-            console.log("Player Wins")
+            alert("Player Wins")
         }
         else if(player.toLowerCase() =="paper"){
-            console.log("Computer Wins")
+            alert("Computer Wins")
         }
         else{
-            console.log("Tie")
+            alert("Tie")
         }
     }
 }
@@ -60,6 +60,6 @@ function game(){
     for (let i = 1; i < 6; i++){
         const result = prompt("Choose either rock, paper, or scissors (round of 5)");
         console.log("Round " + i);
-        RPS(result, compScore, playerScore);
+        RPS(result);
     }
 }
