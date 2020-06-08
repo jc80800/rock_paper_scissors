91bc3b42e5c742c75c96910333dab749f1d863d9
jc8080 <jc8080@rit.edu>
2020-06-08 15:23:50 -0400

    Used querySelector to target existing and elements. Created new element to keep track of victory and score.  Updating consistently after each round. Added CSS for the new div elements. Documentation

--- js/script.js
+++ js/script.js
@@ -4,6 +4,25 @@ Let 2 = Paper
 Let 3 = Scissors
 
 */
+// global variables that keep track of scores
+let compScore = 0;
+let playerScore = 0;
+
+//Uses querySelector to find a div element
+const score = document.querySelector('.score');
+const victory = document.querySelector('.victory');
+
+//Creates a new element that will be inserted
+const scoreBox = document.createElement('scoreBox');
+const victor = document.createElement('victor');
+
+//Set up initial content for the element
+scoreBox.textContent = "Computer: " + compScore + "           ||||| Player: " +playerScore ;
+victor.textContent = "Who will be the winner?";
+
+//Add the newly created element to the existing div element
+score.appendChild(scoreBox);
+victory.appendChild(victor);
 
 
 //Generates Computer's choice
@@ -13,53 +32,63 @@ function generateComputer(){
 }
 
 //Checks player's choice and compares to computer
+//Element's content are updated as the game progresses
+//Scores are incremented as the game progresses
 function RPS(player){
-    let compChoice;
     const compNum = generateComputer();
     if (compNum == 1){
         compChoice = "rock"
         if(player.toLowerCase() == "rock"){
-            alert("tie")  
+            victor.innerHTML = "Tie";
         }
         else if(player.toLowerCase() == "paper"){
-            alert("Player Wins")
+            victor.innerHTML = "Player Wins";
+            playerScore++;
         }
         else{
-            alert("Computer Wins")
+            victor.innerHTML = "Computer Wins";
+            compScore++;
         }
     }
     else if(compNum == 2){
         compChoice = "paper"
         if(player.toLowerCase() == "rock"){
-            alert("Computer Wins")
+            victor.innerHTML = "Computer Wins";
+            compScore++;
         }
         else if(player.toLowerCase() == "paper"){
-            alert("Tie")
+            victor.innerHTML = "Tie";
         }
         else{
-            alert("Player Wins")
+            victor.innerHTML = "Player Wins";
+            playerScore++;
         }
     }
     else{
         compChoice = "scissors"
         if(player.toLowerCase() == "rock"){
-            alert("Player Wins")
+            victor.innerHTML = "Player Wins";
+            playerScore++;
         }
         else if(player.toLowerCase() =="paper"){
-            alert("Computer Wins")
+            victor.innerHTML = "Computer Wins";
+            compScore++;
         }
         else{
-            alert("Tie")
+            victor.innerHTML = "Tie";
         }
     }
+
+    scoreBox.innerHTML = "Computer: " + compScore + "           ||||| Player: " +playerScore ;
 }
 
+//Used to play a game of 5 rounds - no longer used
+/*
 function game(){
-    let compScore = 0;
-    let playerScore = 0;
     for (let i = 1; i < 6; i++){
         const result = prompt("Choose either rock, paper, or scissors (round of 5)");
         console.log("Round " + i);
         RPS(result);
     }
 }
+*/
